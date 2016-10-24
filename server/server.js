8067363585bf3c3b8020fda1cf7970720acd1920
Henry Lyford <hlyford@commonsense.org>
2016-10-23 18:10:48 -0700

    Add shrinkwrap; include postgres

--- server/server.js
+++ server/server.js
@@ -6,12 +6,20 @@ import responseTime from 'response-time'
 import babel from 'babel-core/register'
 
 // require in other files
-var router = require('./routes/router.js');
+const router = require('./routes/router.js');
 
 const port = process.env.PORT || 8050;
 const app = express();
 
 // set up postgres DB
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/mocabulary';
+
+const client = new pg.Client(connectionString);
+client.connect();
+const query = client.query(
+  'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', () => { client.end(); });
 
 // middleware
 app.use(responseTime());
