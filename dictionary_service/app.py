5e2b522ca2e30fb984efc727b5250f3ecd1f7eca
Henry Lyford <hlyford@commonsense.org>
2016-10-26 17:06:51 -0700

    Create /word route on node server and flask server

--- dictionary_service/app.py
+++ dictionary_service/app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 def index():
     return 'Hello world'
 
-@app.route('/henry')
-def stuff():
-    return '<div>hihih</div>sss'
+@app.route('/word/<word>', methods=['GET', 'POST'])
+def word():
+		print 'Calling off to Twilio API...'
+    # return 'Calling off to Twilio API...'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import path from 'path'
+import request from 'request'
+
+// change to env config later
+const pythonUrl = 'http://0.0.0.0:5000/';
 
 let app = require('../server');
 let router = express.Router();
@@ -9,8 +13,26 @@ router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 
-router.route('/').get(function(req, res) {
-	res.send('hello world!')
+router.route('/word').get(function(req, res) {
+	let word = req.query.word;
+	let formData = {word: word};
+	console.log(pythonUrl + word);
+	request.post({url: pythonUrl + 'word' + '/' + word, formData: formData}, (error, response, body) => {
+	  if (!error && response.statusCode == 200) {
+	    console.log(body) // Show the HTML for the Google homepage.
+	    res.send(body);
+	  } else {
+	  	console.log('error dawg: ', response.statusCode);
+	  	res.send('Error with microservice: ', response.statusCode);
+	  }
+	});
 });
+
+// request.post({url:'http://service.com/upload', formData: formData}, function optionalCallback(err, httpResponse, body) {
+//   if (err) {
+//     return console.error('upload failed:', err);
+//   }
+//   console.log('Upload successful!  Server responded with:', body);
+// });
 
 module.exports = router;
