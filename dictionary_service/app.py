052852659bdca79eaf971e33b2b9603df5450b4f
Henry Lyford <hlyford@commonsense.org>
2016-11-05 14:36:19 -0400

    does a lot of stuff

--- dictionary_service/app.py
+++ dictionary_service/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -7,9 +8,12 @@ def index():
     return 'Hello world'
 
 @app.route('/word/<word>', methods=['GET', 'POST'])
-def word():
-		print 'Calling off to Twilio API...'
-    # return 'Calling off to Twilio API...'
+def word(word):
+		print word
+		# send the word to the controller 'receiveWord'
+		# response = receiveWord(word)
+		# return response
+		return word
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='127.0.0.1')
---
+++ dictionary_service/controllers/receiveWord.py
@@ -0,0 +1,11 @@
+def receiveWord(word):
+	# lower case the word
+	# remove white spaces
+
+	# send the word to wordnik API
+	# receive response
+	# make an object for the response
+		# { word: str, defintion: str, sentence: str, partOfSpeech: str }
+
+	# return object to router
+
